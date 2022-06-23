65df64b01e1a77b5ebb3abe7d83826d8a975a4e8
aztaro97 <aztaro97@gmail.com>
2022-06-24 00:38:22 +0400

    Add & Config graphQl client side

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,7 @@
-const colors = require("tailwindcss/colors");
-
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
-    colors,
-  },
+ },
   plugins: [],
 };
